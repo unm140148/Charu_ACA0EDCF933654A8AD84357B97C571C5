7b25ca54abd0a65d918ef805d326cdaf999dc69d
unm140148 <144208760+unm140148@users.noreply.github.com>
2023-09-06 12:19:20 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,6 @@
+#write a program that determines whether a year entered by the user is leap year or not  using if else statements
+number=int(input())
+if number%4==0:
+   print("leap year")
+else:
+   print("not leap year")
